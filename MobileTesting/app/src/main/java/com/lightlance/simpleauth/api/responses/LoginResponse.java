1a3958b600aea88df9a4d3c869c2c9387d01c04f
Adilah Qurrotu'aini <adilah.qurrotu.aini@gmail.com>
2021-12-04 13:23:40 +0700

    del

--- MobileTesting/app/src/main/java/com/lightlance/simpleauth/api/responses/LoginResponse.java
+++
@@ -1,8 +0,0 @@
-package com.lightlance.simpleauth.api.responses;
-
-import com.lightlance.simpleauth.api.model.User;
-
-public class LoginResponse {
-    private String status;
-    private User user;
-}
